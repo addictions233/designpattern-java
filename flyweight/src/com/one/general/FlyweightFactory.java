92f682f060ac066a118fc91680d1f4c498768c07
wanjunjie <wanjunjie@junrunrenli.com>
2024-10-30 22:31:40 +0800

    学习设计模式

--- flyweight/src/com/one/general/FlyweightFactory.java
+++ flyweight/src/com/one/general/FlyweightFactory.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author one
- * @description 获取享元对象的工厂: 享元模式又称轻量级模式, 是对象池的一种实现, 可以避免不停的创建和销毁多个对象, 消耗性能,
+ * @description 获取享元对象的工厂: 享元模式(Flyweight Pattern)又称轻量级模式, 是对象池的一种实现, 可以避免不停的创建和销毁多个对象, 消耗性能,
  * 共享细粒度对象, 将多个对同一对象的访问集中起来
  * @date 2024-10-30
  */
