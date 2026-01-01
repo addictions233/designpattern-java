faf89cdf226d5414d8d2d3b9a386a559a072614a
wanjunjie <ogo1828wan@163.com>
2026-01-01 20:31:29 +0800

    学习设计模式

--- proxy/src/com/one/staticproxy/UserDaoProxy.java
+++ proxy/src/com/one/staticproxy/UserDaoProxy.java
@@ -1,7 +1,8 @@
 package com.one.staticproxy;
 
 /**
- * 实现静态代理
+ * 实现静态代理: 静态代理在编译时就已经实现了, 编译完成之后的代理类就是一个实际的class文件
+ * 动态代理在运行时动态生成的, 即编译完成后没有实际的class文件, 而是运行时动态生成类字节码, 并加载到JVM中
  */
 public class UserDaoProxy implements IUserDao {
 
