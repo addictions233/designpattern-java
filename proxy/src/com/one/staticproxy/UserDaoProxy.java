7bcf78fb23d6652ed70d2c84d28771943f957f58
wanjunjie <ogo1828wan@163.com>
2026-01-01 20:28:49 +0800

    学习设计模式

--- proxy/src/com/one/staticproxy/UserDaoProxy.java
+++ proxy/src/com/one/staticproxy/UserDaoProxy.java
@@ -1,7 +1,13 @@
 package com.one.staticproxy;
 
+/**
+ * 实现静态代理
+ */
 public class UserDaoProxy implements IUserDao {
 
+    /**
+     * 持有目标对象
+     */
     private IUserDao target;
 
     public UserDaoProxy(IUserDao target) {
@@ -10,7 +16,7 @@ public class UserDaoProxy implements IUserDao {
 
     @Override
     public void save() {
-        System.out.println("开启事务");
+        System.out.println("开启事务"); // 对目标对象进行增强
         target.save();
         System.out.println("提交事务");
     }
