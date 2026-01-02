2c411da367f9c76008028d0d243830f92f125780
wanjunjie <ogo1828wan@163.com>
2026-01-02 21:04:42 +0800

    学习设计模式

--- proxy/src/com/one/jdkproxy/ProxyFactoryTest.java
+++ proxy/src/com/one/jdkproxy/ProxyFactoryTest.java
@@ -14,5 +14,8 @@ public class ProxyFactoryTest {
         IUserDao proxy = (IUserDao)new ProxyFactory(userDao).getProxy();
         System.out.println(proxy.getClass());
         proxy.save();
+        System.out.println("==================================");
+        // 代理逻辑对于所有的代理对象都生效
+        proxy.toString();
     }
 }
